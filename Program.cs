25391cf2f57d61e0ccb6452eba8853fe6742f13c
khalid karam <khalid.tasks@gmail.com>
2024-12-15 23:30:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LinqGenerationMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var collection = Enumerable.Empty<int>();
+            //var collection = Enumerable.Repeat(1, 20);
+            var collection = Enumerable.Range(1, 20).ToList();
+
+            collection.Add(52);
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
